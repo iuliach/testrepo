b221b2668e1243344c3ca67f8ce909f5ffb4c667
iuliach <147597590+iuliach@users.noreply.github.com>
2025-07-24 12:01:23 +0200

    Update example.spec.js

--- example.spec.js
+++ example.spec.js
@@ -1,37 +1,7 @@
-/*
-const { test, expect } = require('@playwright/test');
-
-test.only('example.com loads and contains expected text', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example Domain/);
-  await expect(page.locator('h1')).toHaveText('Example Domain');
-});
-*/
-
-/*const {test} = require('@playwright/test');
-
-
-test('First PWtest',async ()=> //anonymus function
-{
-//playwright code
-//step1 - open brwser
-//step2 - login - 2 seconds
-//await
-//step3 - click
-
-
-});
-*/
-
-
 const {test , expect} = require('@playwright/test');
-//const {expect} = require('../playwright.config');
-//const { clear } = require('console');
-
 
 test.only('Browser login',async ({page})=>
 {
-//chrome - plugins/cookies
 const username = "student";
 //const text = page.locator("p[class='has-text-align-center'] strong");
 //const button = page.locator("//a[normalize-space()='Log out']");
@@ -47,15 +17,12 @@ await page.waitForLoadState('networkidle');
 const titles = await page.locator(".post-title").allTextContents();
 console.log(titles);
     //await expect(page.locator("p[class='has-text-align-center'] strong").last()).toBeChecked();
-
-
-
-
 await page.waitForTimeout(5000); 
 
 
 });
 
+/*
 test('Browser negative login1',async ({page})=>
 {
 //chrome - plugins/cookies
@@ -77,6 +44,3 @@ await page.waitForTimeout(5000);
 });
 
 
-
-//const cardTitles = page.locator('.card-body b');
-//console.log(await page.title());
